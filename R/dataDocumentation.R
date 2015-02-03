ba43d0a01c79a006b68b3a20ddb54a76aec6783f
stefvanlieshout <stefvanlieshout@fastmail.fm>
2015-02-03 14:35:31 +0100

    dev

---
+++ R/dataDocumentation.R
@@ -0,0 +1,113 @@
+#' LGG150 cghCall object
+#'
+#' A dataset containing the copynumber, segment and call
+#'  data for chromosomes 7, 8, 9 and 10 of a Low Grade 
+#'  Glioma sample (LGG150).
+#'
+#' For more info on the cghCall class, see the package
+#'  "CGHcall"
+#'
+#' @docType data
+#' @keywords datasets
+#' @name LGG150.data
+#' @usage data( LGG150.data )
+#' @format An object of class "cghCall"
+NULL
+
+#' Gene Annotation hg19
+#'
+#' A dataset containing the gene locations
+#'  required for the BreakPointGenes package.
+#'
+#' \itemize{
+#'   \item Gene (gene name)
+#'   \item EnsID (ensembl gene id)
+#'   \item Chromosome
+#'   \item Start (Genomic start of gene)
+#'   \item End (Genomic end of gene)
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name gene.annotation.hg19
+#' @usage data( gene.annotation.hg19 )
+#' @format A data.frame
+NULL
+
+#' Gene Annotation hg19 chr18
+#'
+#' A dataset containing the gene locations
+#'  required for the BreakPointGenes package.
+#'
+#' See help( gene.annotation.hg19 ) for more info on contents
+#' @docType data
+#' @keywords datasets
+#' @name gene.annotation.hg19.chr18
+#' @usage data( gene.annotation.hg19.chr18 )
+#' @format A data.frame
+NULL
+
+#' Gene Annotation hg19 chr20
+#'
+#' A dataset containing the gene locations
+#'  required for the BreakPointGenes package.
+#'
+#' See help( gene.annotation.hg19 ) for more info on contents
+#' @docType data
+#' @keywords datasets
+#' @name gene.annotation.hg19.chr20
+#' @usage data( gene.annotation.hg19.chr20 )
+#' @format A data.frame
+NULL
+
+#' Gene Annotation hg19 chr21
+#'
+#' A dataset containing the gene locations
+#'  required for the BreakPointGenes package.
+#'
+#' See help( gene.annotation.hg19 ) for more info on contents
+#' @docType data
+#' @keywords datasets
+#' @name gene.annotation.hg19.chr21
+#' @usage data( gene.annotation.hg19.chr21 )
+#' @format A data.frame
+NULL
+
+#' CGHcall CAIRO data hg19 chr18
+#'
+#' A test dataset containing chr18 copynumber data
+#'  for the BreakPointGenes package. For more info about the
+#'  CAIRO study see...
+#'
+#' @docType data
+#' @keywords datasets
+#' @name copynumber.data.chr18
+#' @usage data( copynumber.data.chr18 )
+#' @format A data.frame
+NULL
+
+#' CGHcall CAIRO data hg19 chr20
+#'
+#' A test dataset containing chr20 copynumber data
+#'  for the BreakPointGenes package. For more info about the
+#'  CAIRO study see...
+#'
+#' @docType data
+#' @keywords datasets
+#' @name copynumber.data.chr20
+#' @usage data( copynumber.data.chr20 )
+#' @format A data.frame
+NULL
+
+#' CGHcall CAIRO data hg19 chr21
+#'
+#' A test dataset containing chr21 copynumber data
+#'  for the BreakPointGenes package. For more info about the
+#'  CAIRO study see...
+#'
+#' @docType data
+#' @keywords datasets
+#' @name copynumber.data.chr21
+#' @usage data( copynumber.data.chr21 )
+#' @format A data.frame
+NULL
